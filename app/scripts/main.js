280a8a070ca3aa5e500dda33f6c15eada2df9ac4
havengarfx <havengarfx@gmail.com>
2020-07-22 21:30:17 +0000

    init script form

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
+document.addEventListener("load", start);
+let inputSearch = document.addEventListener(keyup, "#input-search");
 
+function start() {
+  preventFormSubmit();
+  
+}
+
+function preventFormSubmi(){
+  function handleFormSubmit(event) {
+   
+  }
+  
+}
