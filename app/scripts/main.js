347d5eec447826d015db874376dca9d983f23934
havengarfx <havengarfx@gmail.com>
2020-07-22 23:50:52 +0000

    Live server

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,23 @@
 document.addEventListener("load", start);
-let inputSearch = document.addEventListener(keyup, "#input-search");
+let inputSearch = null;
 
 function start() {
+  console.log("start");
+  inputSearch = document.querySelector("#input-search");
+  console.log(inputSearch);
   preventFormSubmit();
-  
+  activateInput();
 }
 
-function preventFormSubmi(){
+function preventFormSubmit() {
   function handleFormSubmit(event) {
-   
+    event.preventDefault();
   }
-  
+  let formSearchUsers = document.querySelector("form");
+  formSearchUsers.addEventListener("submit", handleFormSubmit);
+}
+
+function activateInput() {
+  inputSearch.focus();
+  inputSearch.addEventListener("keyup", handleFormTyping);
 }
